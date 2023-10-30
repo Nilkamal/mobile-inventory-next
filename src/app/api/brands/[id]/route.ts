3e1cffefd3e1deffc5a46f60bab2a30f8b82164b
Nilkamal Shah <nilshah@WKMZTF9A7B64.global.publicisgroupe.net>
2023-10-30 17:02:41 +0530

    fix: completed brand screens

--- src/app/api/brands/[id]/route.ts
+++ src/app/api/brands/[id]/route.ts
@@ -6,5 +6,17 @@ export async function GET(request: any, { params }: any) {
   const { id } = params;
   await connect();
   const brand = await Brand.findById(id);
-  return NextResponse.json({ brand }, { status: 200 });
+  return NextResponse.json(brand, { status: 200 });
+}
+
+export async function PUT(request: any, { params }: any) {
+  const { id } = params;
+  const { brand } = await request.json();
+  await connect();
+  await Brand.findByIdAndUpdate(id, { brand });
+
+  return NextResponse.json(
+    { message: "Brand details updated!" },
+    { status: 200 }
+  );
 }
